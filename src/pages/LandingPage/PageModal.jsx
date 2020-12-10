e00abd5e6be271d07fc5cfdc273e27b6001c83cd
Tom <tom@hello-air.com>
2020-12-10 22:22:05 +0800

    update copy field

--- src/pages/LandingPage/PageModal.jsx
+++ src/pages/LandingPage/PageModal.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
+import Spritesheet from 'react-responsive-spritesheet';
 import { config } from '../../UtilHelpers';
 import CopyToShare from '../SharePage/CopyToShare';
 
+const StartBtnAni = `${config.assetsUrl}/start_btn_ani.png`;
 const ErrorMotionSensor = `${config.assetsUrl}/error_motion_sensor.png`;
 const Arrow = `${config.assetsUrl}/selfie_result_cta_arrow.png`;
 const Safari = `${config.assetsUrl}/safari.svg`;
@@ -42,11 +44,24 @@ const PageModal = (props) => {
         <div className="must-in-safari">
           <img id="safari-info" src={Safari} />
           <p>在浏览器中<br />打开链接开始游戏</p>
-          <div id="site-link">https://skinceuticalstrasia.cn</div>
+          <div id="site-link">https://skinceuticalstrasia.cn/aoxmobilegame2020</div>
           <CopyToShare target="site-link" msgToCopy="https://skinceuticalstrasia.cn/aoxmobilegame2020/">
-            <div className="start-btn">
+            {/* <div className="start-btn">
               <img className="start-btn-img" src={BtnAni} />
               <p>复制链接</p>
+            </div> */}
+            <div className="start-game-btn">
+              <Spritesheet
+                image={StartBtnAni}
+                widthFrame={402}
+                heightFrame={122}
+                steps={60}
+                fps={45}
+                autoplay
+                loop
+                isResponsive
+              />
+              <p>开始游戏</p>
             </div>
           </CopyToShare>
         </div>
